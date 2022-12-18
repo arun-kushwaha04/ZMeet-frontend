4bfcb4c2a8d3f0e425274d9ba5cac0d173ece3bc
Arun Singh Kushwaha <arunkushwaha89765@gmail.com>
2022-12-18 13:47:38 +0530

    Updated Urls

--- src/urls.js
+++ src/urls.js
@@ -2,8 +2,8 @@
 // const BACKEND_URL = 'http://localhost:8000';
 // const HOSTNAME = '127.0.0.1';
 
-const BACKEND_URL = `https://zmeet-dawn.herokuapp.com`;
-const PORT = 41252;
+const BACKEND_URL = `https://zmeet.onrender.com`;
+const PORT = 10000;
 const HOSTNAME = 'zmeet-dawn.herokuapp.com';
 
 export { BACKEND_URL, HOSTNAME, PORT };
